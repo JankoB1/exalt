72b9c4600a50eb411b9b0e0137ee2063eb9ef039
Dimitrije <dimitrije.kolasinac@gmail.com>
2024-03-09 21:24:11 +0100

    Diman nesto cacko

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./components/Header/Header";
+import MainBanner from './components/MainBanner/MainBanner';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainBanner />
     </div>
   );
 }
---
+++ src/components/MainBanner/MainBanner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MainBanner.css'
+
+
+const MainBanner: React.FC = () => {
+    return (
+        <div className="main-banner">
+            <div className='main-banner-img'>
+            </div>
+        </div>
+    );
+};
+
+export default MainBanner;

6140c480ccf9074d138808f0fbfe54c6f79460b9
Janko Blagojevic <janko.tbbt@gmail.com>
2024-03-09 20:38:41 +0100

    Init

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <h1>This is the Header</h1>
+        </header>
+    );
+};
+
+export default Header;
